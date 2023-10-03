9aa458904de1ee15b555b9cec6ff32f31edb9c65
Rudy Brandies <adanb82@gmail.com>
2023-10-03 13:06:45 -0500

    added function to retrieve lyrics from api

--- script.js
+++ script.js
@@ -40,7 +40,19 @@ async function getMoreSongs(url) {
     const data = await res.json();
   
     showData(data);
-  }
+}
+
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>`;
+
+    more.innerHTML = '';
+}
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -52,5 +64,16 @@ form.addEventListener('submit', e => {
     }
     else {
     searchSongs(searchTerm);
+    }
+});
+
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+
+    if(clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle');
+
+        getLyrics(artist, songTitle);
     }
 });
